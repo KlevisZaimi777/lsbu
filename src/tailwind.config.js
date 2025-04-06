12fe547c2c755462f581a237f122a42c6abd7cd4
Klevis Zaimi <kleviszaimi73@gmail.com>
2025-04-06 15:47:56 +0100

    Your commit message

---
+++ src/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js,jsx,ts,tsx}',  // Ensure paths are correct
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
