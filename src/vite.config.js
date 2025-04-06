9667c70de54f21fb1fd199dbd89f5f3df2dc2576
Klevis Zaimi <kleviszaimi73@gmail.com>
2025-04-06 17:12:32 +0100

    Fix build configuration

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  base: './', // Use relative paths
   build: {
-    outDir: 'dist', // Ensure that the output directory is 'dist'
-    rollupOptions: {
-      input: 'index.html', // Ensure the build input is correct
-    },
-  },
+    outDir: 'dist', // Explicit output directory
+    emptyOutDir: true, // Clear directory on rebuild
+  }
 });
-
